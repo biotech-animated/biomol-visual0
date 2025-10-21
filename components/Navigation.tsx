7420b08191a2a47a415a722901974e470e7ec6de
biotech-animated <dev@biotech-animated.com>
2025-10-22 04:35:14 +0900

    Updated Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -50,7 +50,7 @@ export default function Navigation() {
                 fontWeight: '500'
               }}
             >
-              <span style={{ color: 'var(--bmv-purple)' }}>BIOMOL</span>
+              <span style={{ color: 'var(--bmv-purple)' }}>BIOMOL </span>
               <span style={{ color: 'var(--bmv-orange)' }}>VISUAL</span>
             </span>
           </Link>

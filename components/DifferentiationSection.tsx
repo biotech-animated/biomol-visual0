f04aedb08ae2ebe3926f4e4d59f43f7cf52affa6
michel-adelino <micheladelino222@gmail.com>
2025-10-22 07:54:01 +0900

    fix padding for text

--- components/DifferentiationSection.tsx
+++ components/DifferentiationSection.tsx
@@ -72,7 +72,7 @@ export default function DifferentiationSection() {
                   </p>
                 </div>
 
-                <p className="text-responsive-sm" style={{ fontFamily: "'Red Hat Text', sans-serif", fontWeight: '400', color: 'var(--bmv-purple-light)' }}>
+                <p className="text-responsive-sm pb-6" style={{ fontFamily: "'Red Hat Text', sans-serif", fontWeight: '400', color: 'var(--bmv-purple-light)' }}>
                   Just a smarter process. And a more powerful result.
                 </p>
               </div>

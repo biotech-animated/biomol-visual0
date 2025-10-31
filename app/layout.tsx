4416660ee30b6b3a0288585961b4b1994d6b98b1
biotech-animated <dev@biotech-animated.com>
2025-11-01 06:45:10 +0900

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,31 +74,116 @@ export default function RootLayout({
               "@context": "https://schema.org",
               "@type": "ProfessionalService",
               "name": "Biomol Visual",
-              "description": "Molecular & Cellular MOA Visualization That Gets You Funded",
+              "alternateName": "Biotech Animated",
+              "foundingDate": "2015-01-01",
+              "slogan": "Molecular & Cellular MOA Visualization That Gets You Funded",
+              "description": "Molecular & Cellular MOA Visualization That Gets You Funded. We translate complex molecular science into compelling visual stories designed to win investor confidence.",
               "url": "https://biomolvisual.com",
               "logo": "https://biomolvisual.com/favicon.ico",
               "areaServed": "Worldwide",
+              "numberOfEmployees": 10,
               "serviceType": "Medical Animation Services",
+              "sameAs": [
+                "https://www.linkedin.com/company/biotechanimated",
+                "https://twitter.com/biotechanimated",
+                "https://www.youtube.com/@BiotechAnimated",
+                "https://www.instagram.com/biotechanimated/"
+              ],
               "knowsAbout": [
                 "Molecular Mechanism of Action Visualization",
                 "Cellular Biology Visualization",
                 "Biotechnology Communication",
                 "Scientific Animation",
-                "Pharmaceutical Visualization"
+                "Pharmaceutical Visualization",
+                "Mechanism of Disease (MOD) Visualization",
+                "Biotherapeutic Mechanism Visualization",
+                "Platform Technology Visualization"
               ],
-              "offers": {
-                "@type": "Offer",
-                "itemOffered": {
-                  "@type": "Service",
-                  "name": "MOA Visualization Services",
-                  "description": "Professional molecular and cellular visualization services for biotech companies"
+              "offers": [
+                {
+                  "@type": "Offer",
+                  "itemOffered": {
+                    "@type": "Service",
+                    "name": "Molecular & Cellular MOA Visualization",
+                    "description": "Scientifically precise molecular and cellular mechanism of action visualizations for biotech companies"
+                  }
+                },
+                {
+                  "@type": "Offer",
+                  "itemOffered": {
+                    "@type": "Service",
+                    "name": "Mechanism of Disease (MOD) Visualization",
+                    "description": "Clear visualization of disease mechanisms for investor and partner communications"
+                  }
+                },
+                {
+                  "@type": "Offer",
+                  "itemOffered": {
+                    "@type": "Service",
+                    "name": "Biotherapeutic Mechanism Visualization",
+                    "description": "Visual storytelling for biotherapeutic drug mechanisms and development"
+                  }
+                },
+                {
+                  "@type": "Offer",
+                  "itemOffered": {
+                    "@type": "Service",
+                    "name": "Platform Technology Visualization",
+                    "description": "Visualization of innovative platform technologies for biotech companies"
+                  }
+                },
+                {
+                  "@type": "Offer",
+                  "itemOffered": {
+                    "@type": "Service",
+                    "name": "Scientific Storytelling",
+                    "description": "Transforming complex science into compelling narratives for fundraising and partnerships"
+                  }
+                },
+                {
+                  "@type": "Offer",
+                  "itemOffered": {
+                    "@type": "Product",
+                    "name": "SLAB - Seamless Loop Asset Bank",
+                    "description": "Premium looping molecular and cellular assets ready to customize and deploy in weeks"
+                  }
                 }
+              ],
+              "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "5",
+                "bestRating": "5",
+                "ratingCount": "70",
+                "reviewCount": "70"
               },
               "contactPoint": {
                 "@type": "ContactPoint",
                 "contactType": "Customer Service",
-                "email": "info@biomolvisual.com"
-              }
+                "email": "info@biomolvisual.com",
+                "availableLanguage": "English"
+              },
+              "additionalProperty": [
+                {
+                  "@type": "PropertyValue",
+                  "name": "Total Client Funding Raised",
+                  "value": "$500M+"
+                },
+                {
+                  "@type": "PropertyValue",
+                  "name": "Successful Funding Rounds",
+                  "value": "30+"
+                },
+                {
+                  "@type": "PropertyValue",
+                  "name": "Total Clients Served",
+                  "value": "70+"
+                },
+                {
+                  "@type": "PropertyValue",
+                  "name": "Previous Brand Name",
+                  "value": "Biotech Animated (rebranded to Biomol Visual in October 2025)"
+                }
+              ]
             })
           }}
         />
